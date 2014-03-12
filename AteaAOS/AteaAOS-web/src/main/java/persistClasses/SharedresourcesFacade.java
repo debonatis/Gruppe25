772de6704c018a8f6656646877272f6b40eaa206
simond <simond@13.37.10.12>
2014-03-12 17:34:02 +0100

    Merge origin/master
    Conflicts:
    	AteaAOS/AteaAOS-web/faces-config.NavData
    	AteaAOS/AteaAOS-web/nb-configuration.xml

---
+++ AteaAOS/AteaAOS-web/src/main/java/persistClasses/SharedresourcesFacade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package persistClasses;
+
+import Entitys.Sharedresources;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author simond
+ */
+@Stateless
+public class SharedresourcesFacade extends AbstractFacade<Sharedresources> {
+    @PersistenceContext(unitName = "com.Gruppe25_AteaAOS-web_war_1.0-SNAPSHOTPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public SharedresourcesFacade() {
+        super(Sharedresources.class);
+    }
+    
+}
