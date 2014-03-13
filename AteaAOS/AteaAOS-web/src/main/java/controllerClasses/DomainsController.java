a382b644d9423cb1da87c6d151e0cab409becafe
simond <simond@13.37.10.12>
2014-03-13 07:53:17 +0100

    Merge origin/master
    Conflicts:
    	AteaAOS/AteaAOS-web/faces-config.NavData
    	AteaAOS/AteaAOS-web/nb-configuration.xml

--- AteaAOS/AteaAOS-web/src/main/java/controllerClasses/DomainsController.java
+++ AteaAOS/AteaAOS-web/src/main/java/controllerClasses/DomainsController.java
@@ -7,18 +7,22 @@ import persistClasses.DomainsFacade;
 
 import java.io.Serializable;
 import java.util.ResourceBundle;
+import java.util.logging.Logger;
 import javax.ejb.EJB;
-import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
 import javax.faces.convert.FacesConverter;
+import javax.faces.event.ActionEvent;
 import javax.faces.model.DataModel;
 import javax.faces.model.ListDataModel;
 import javax.faces.model.SelectItem;
+import org.primefaces.event.FlowEvent;
 
-@Named("domainsController")
+@ManagedBean(name ="domainsController")
 @SessionScoped
 public class DomainsController implements Serializable {
 
@@ -28,9 +32,41 @@ public class DomainsController implements Serializable {
     private persistClasses.DomainsFacade ejbFacade;
     private PaginationHelper pagination;
     private int selectedItemIndex;
+   
+      
+    private boolean skip;  
+      
+    private static Logger logger = Logger.getLogger(DomainsController.class.getName());  
 
     public DomainsController() {
     }
+    public void save(ActionEvent actionEvent) {  
+        create();
+          
+        FacesMessage msg = new FacesMessage("Domene er laget", "Domenet var :" + current);  
+        FacesContext.getCurrentInstance().addMessage(null, msg);  
+    }  
+      
+    public boolean isSkip() {  
+        return skip;  
+    }  
+  
+    public void setSkip(boolean skip) {  
+        this.skip = skip;  
+    }  
+      
+    public String onFlowProcess(FlowEvent event) {  
+        logger.info("Current wizard step:" + event.getOldStep());  
+        logger.info("Next step:" + event.getNewStep());  
+          
+        if(skip) {  
+            skip = false;   
+            return "confirm";  
+        }  
+        else {  
+            return event.getNewStep();  
+        }  
+    } 
 
     public Domains getSelected() {
         if (current == null) {
