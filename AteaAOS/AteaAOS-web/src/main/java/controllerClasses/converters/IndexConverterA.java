bdba946da0c5faf30c6d02c9b74b08e23775d83e
Martin <Martin@martin>
2014-04-08 15:10:53 +0200

    Merge origin/master
    Conflicts:
    	AteaAOS/AteaAOS-web/src/main/webapp/secure/adminPage.xhtml

---
+++ AteaAOS/AteaAOS-web/src/main/java/controllerClasses/converters/IndexConverterA.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllerClasses.converters;
+
+import entityModels.Applications;
+import entityModels.Groups;
+import javax.ejb.EJB;
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.inject.Named;
+import persistClasses.ApplicationsFacade;
+import persistClasses.GroupsFacade;
+
+/**
+ *
+ * @author Martin
+ */
+@Named
+@ApplicationScoped
+public class IndexConverterA implements Converter {
+
+    private Applications app;
+    @EJB
+    private ApplicationsFacade applicationsEJB;
+
+    @Override
+    public Object getAsObject(FacesContext context, UIComponent component, String value) {
+        
+        return applicationsEJB.find(value);
+    }
+
+    @Override
+    public String getAsString(FacesContext context, UIComponent component, Object value) {
+        app = (Applications) value;
+
+        return app.getApplicationid();
+
+    }
+
+}
