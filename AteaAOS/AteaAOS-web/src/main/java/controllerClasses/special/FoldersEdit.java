f74cbf1adac6725ee1d55edb1dc4327c9fac37f2
simond <simond@13.37.10.15>
2014-04-28 19:17:20 +0200

    Merge origin/master
    Conflicts:
    	AteaAOS/AteaAOS-web/src/main/webapp/secure/applicationUsers.xhtml

--- AteaAOS/AteaAOS-web/src/main/java/controllerClasses/special/FoldersEdit.java
+++ AteaAOS/AteaAOS-web/src/main/java/controllerClasses/special/FoldersEdit.java
@@ -6,10 +6,59 @@
 
 package controllerClasses.special;
 
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import org.primefaces.model.DefaultTreeNode;
+import org.primefaces.model.TreeNode;
+
 /**
  *
  * @author simond
  */
+@ManagedBean
+@ViewScoped
 public class FoldersEdit {
-    
+     private TreeNode root;  
+  
+    private TreeNode selectedNode;  
+    private String folderName = "new";
+  
+    public FoldersEdit() {  
+        root = new DefaultTreeNode("Root", null);  
+        
+    }  
+  
+    public TreeNode getRoot() {  
+        return root;  
+    }  
+  
+    public TreeNode getSelectedNode() {  
+        return selectedNode;  
+    }  
+  
+    public void setSelectedNode(TreeNode selectedNode) {  
+        this.selectedNode = selectedNode;  
+    }  
+      
+    public void displaySelectedSingle() {  
+        if(selectedNode != null) {  
+            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Selected", selectedNode.getData().toString());  
+  
+            FacesContext.getCurrentInstance().addMessage(null, message);  
+        }  
+    }  
+  
+    public void deleteNode() {  
+        selectedNode.getChildren().clear();  
+        selectedNode.getParent().getChildren().remove(selectedNode);  
+        selectedNode.setParent(null);  
+          
+        selectedNode = null;  
+    } 
+    public void addNode(){
+        TreeNode a = new DefaultTreeNode(folderName, selectedNode);
+        selectedNode = null;
+    }
 }
