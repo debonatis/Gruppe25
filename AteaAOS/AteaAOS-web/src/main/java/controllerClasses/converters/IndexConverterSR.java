387142f778ba71417c96f229ffde5aeaf4af0169
Martin <Martin@martin>
2014-05-14 18:10:50 +0200

    Merge origin/master
    Conflicts:
    	AteaAOS/AteaAOS-web/src/main/webapp/WEB-INF/faces-config.xml
    	AteaAOS/AteaAOS-web/src/main/webapp/adminSecure/admin.xhtml
    	AteaAOS/AteaAOS-web/src/main/webapp/secure/template/footer.xhtml

---
+++ AteaAOS/AteaAOS-web/src/main/java/controllerClasses/converters/IndexConverterSR.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllerClasses.converters;
+
+import entityModels.Groups;
+import entityModels.GroupsPK;
+import entityModels.Sharedresources;
+import entityModels.SharedresourcesPK;
+import javax.ejb.EJB;
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.inject.Named;
+import persistClasses.GroupsFacade;
+import persistClasses.SharedresourcesFacade;
+
+/**
+ *
+ * @author simond
+ */
+@Named
+@ApplicationScoped
+public class IndexConverterSR implements Converter {
+
+    private Sharedresources gruppe;
+    @EJB
+    private SharedresourcesFacade sharedresourceEJB;
+
+    @Override
+    public Object getAsObject(FacesContext context, UIComponent component, String value) {
+        Sharedresources g = new Sharedresources(new SharedresourcesPK(value, (String) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("projectID")));
+        return sharedresourceEJB.find(g.getSharedresourcesPK());
+    }
+
+    @Override
+    public String getAsString(FacesContext context, UIComponent component, Object value) {
+        gruppe = (Sharedresources) value;
+
+        return gruppe.getSharedresourcesPK().getDisplaynameshared();
+
+    }
+
+}
