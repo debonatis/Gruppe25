3a66248ac136dc9e4a3dce2935f503b7a16cc746
simond <simond@trl189155.ka.hist.no>
2014-05-14 18:15:44 +0200

    Merge origin/master
    Conflicts:
    	AteaAOS/AteaAOS-web/src/main/java/entityModels/Users.java

--- AteaAOS/AteaAOS-web/src/main/java/persistClasses/GroupusersFacade.java
+++ AteaAOS/AteaAOS-web/src/main/java/persistClasses/GroupusersFacade.java
@@ -7,6 +7,7 @@
 package persistClasses;
 
 import entityModels.Groupusers;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -27,6 +28,14 @@ public class GroupusersFacade extends AbstractFacade<Groupusers> {
 
     public GroupusersFacade() {
         super(Groupusers.class);
+    }
+    @Override
+    public List<Groupusers> findAllPro(String projectID) {
+        
+        javax.persistence.Query q = getEntityManager().createNamedQuery(""+Groupusers.class.getName().substring(13)+".findByProjectidg", Groupusers.class);
+        q.setParameter("projectidg", projectID);
+       List<Groupusers> res = q.getResultList();
+        return res;
     }
     
 }
