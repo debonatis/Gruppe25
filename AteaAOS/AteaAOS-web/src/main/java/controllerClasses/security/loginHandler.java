530e9aa5ef577a283186e9ea7a0726c233657ef2
simond <simond@trl167057.ka.hist.no>
2014-05-19 18:25:48 +0200

    Merge origin/master
    Conflicts:
    	AteaAOS/AteaAOS-web/src/main/java/entityModels/Users.java

---
+++ AteaAOS/AteaAOS-web/src/main/java/controllerClasses/security/loginHandler.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllerClasses.security;
+
+import javax.ejb.Stateless;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ *
+ * @author simond
+ */
+@Stateless
+@Named
+public class loginHandler {
+
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String login () {
+    FacesContext context = FacesContext.getCurrentInstance();
+    HttpServletRequest request = (HttpServletRequest) 
+        context.getExternalContext().getRequest();
+    try {
+      request.login(this.username, this.password);
+    } catch (ServletException e) {
+   
+      context.addMessage(null, new FacesMessage("Login failed."));
+      return "error";
+    }
+    return "/secure/adminPage.xhtml";
+  }
+     public void logout() {
+    FacesContext context = FacesContext.getCurrentInstance();
+    HttpServletRequest request = (HttpServletRequest) 
+        context.getExternalContext().getRequest();
+    try {
+      request.logout();
+    } catch (ServletException e) {
+    
+      context.addMessage(null, new FacesMessage("Logout failed."));
+    }
+  }
+}
