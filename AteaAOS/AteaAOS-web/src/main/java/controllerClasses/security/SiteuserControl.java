e3d56d69dc4d24d513db5b06292132797e093da3
simond <simond@trl167057.ka.hist.no>
2014-05-19 16:22:22 +0200

    Merge origin/master
    Conflicts:
    	AteaAOS/AteaAOS-web/src/main/java/entityModels/Users.java

--- AteaAOS/AteaAOS-web/src/main/java/controllerClasses/security/SiteuserControl.java
+++ AteaAOS/AteaAOS-web/src/main/java/controllerClasses/security/SiteuserControl.java
@@ -8,11 +8,18 @@ package controllerClasses.security;
 import entityModels.City;
 import entityModels.Roles;
 import entityModels.Siteuser;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.Resource;
 import javax.annotation.security.DeclareRoles;
 import javax.annotation.security.RolesAllowed;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.mail.MessagingException;
+import javax.naming.NamingException;
+import javax.servlet.http.HttpServletRequest;
 import persistClasses.CityFacade;
 import persistClasses.RolesFacade;
 import persistClasses.SiteuserFacade;
@@ -25,6 +32,9 @@ import persistClasses.SiteuserFacade;
 @ViewScoped
 @DeclareRoles({"admin"})
 public class SiteuserControl {
+
+    @Resource(name = "mail/AOSMail")
+    private javax.mail.Session mailAOSMail;
 
     private Siteuser user;
     private Roles roleObject;
@@ -71,4 +81,25 @@ public class SiteuserControl {
 
         }
     }
+
+    @RolesAllowed("admin")
+    public void sendInvite() {
+        HttpServletRequest req = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+        String url = req.getRequestURL().toString();
+        url = url.substring(0, url.length() - req.getRequestURI().length()) + req.getContextPath() + "/";
+        try {
+            sendMail(user.getEmail(), "AOS user generation (Do not repley)", url);
+        } catch (NamingException | MessagingException ex) {
+            Logger.getLogger(SiteuserControl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private void sendMail(String email, String subject, String body) throws NamingException, javax.mail.MessagingException {
+        javax.mail.internet.MimeMessage message = new javax.mail.internet.MimeMessage(mailAOSMail);
+        message.setSubject(subject);
+        message.setRecipients(javax.mail.Message.RecipientType.TO, javax.mail.internet.InternetAddress.parse(email, false));
+        message.setText(body);
+        javax.mail.Transport.send(message);
+    }
+
 }
