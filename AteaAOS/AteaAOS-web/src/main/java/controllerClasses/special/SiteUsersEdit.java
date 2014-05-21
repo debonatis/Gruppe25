41a80f1e9f8cf76255e2a20db3fc68bb06b3f73a
simond <simond@trl167057.ka.hist.no>
2014-05-21 11:20:31 +0200

    Merge origin/master
    Conflicts:
    	AteaAOS/AteaAOS-web/src/main/java/entityModels/Users.java

--- AteaAOS/AteaAOS-web/src/main/java/controllerClasses/special/SiteUsersEdit.java
+++ AteaAOS/AteaAOS-web/src/main/java/controllerClasses/special/SiteUsersEdit.java
@@ -63,7 +63,7 @@ import persistClasses.UsersFacade;
 @ViewScoped
 
 public class SiteUsersEdit implements Serializable {
-
+    
     @EJB
     private SiteuserFacade siteuserEJB;
     @EJB
@@ -109,38 +109,38 @@ public class SiteUsersEdit implements Serializable {
     private DualListModel<Siteuser> listSU;
     private List<ProjectUsersModel> proUsers = new ArrayList<>();
     private String[] projectTypes = {"V4", "Single Forrest, Single Domain", "Single Forrest, Multiple Domain"};
-
+    
     public Siteuser getSelected() {
         return selected;
     }
-
+    
     public void setSelected(Siteuser selected) {
-
+        
         this.selected = selected;
-
+        
     }
-
+    
     public List<ProjectUsersModel> getProUsers() {
         return proUsers;
     }
-
+    
     public void setProUsers(List<ProjectUsersModel> proUsers) {
         this.proUsers = proUsers;
     }
-
+    
     public String[] getProjectTypes() {
         return projectTypes;
     }
-
+    
     public void setProjectTypes(String[] projectTypes) {
         this.projectTypes = projectTypes;
     }
-
+    
     @PostConstruct
     public void init() {
-
+        
         projectListT = siteuserEJB.findAll();
-
+        
         selectList = new SiteuserListModel(projectListT);
         listPro = new DualListModel<>(projectsFacade.findAll(), new ArrayList<Projects>());
         listSU = new DualListModel<>(siteuserEJB.findAll(), new ArrayList<Siteuser>());
@@ -151,45 +151,49 @@ public class SiteUsersEdit implements Serializable {
             for (Prositeusers gu : prositeusersFacade.findAll()) {
                 if (gu.getPrositeusersPK().getProjectid().equalsIgnoreCase(g.getProjectid())) {
                     roger.add(siteuserEJB.find(gu.getPrositeusersPK().getUsername()));
-
+                    
                 }
             }
             proUsers.add(new ProjectUsersModel(g, roger));
         }
     }
-
+    
     public DualListModel<Projects> getListPro() {
         return listPro;
     }
-
+    
     public void setListPro(DualListModel<Projects> listPro) {
         this.listPro = listPro;
     }
-
+    
     public DualListModel<Siteuser> getListSU() {
         return listSU;
     }
-
+    
     public void setListSU(DualListModel<Siteuser> listSU) {
         this.listSU = listSU;
     }
-
+    
     public SiteuserListModel getSelectList() {
         return selectList;
     }
-
+    
     public void setSelectList(SiteuserListModel selectList) {
         this.selectList = selectList;
     }
-
+    
     public void deleteItemUsr(Siteuser e) {
-
+        for (Prositeusers psu : prositeusersFacade.findAll()) {
+            if (psu.getPrositeusersPK().getUsername().equalsIgnoreCase(e.getUsername())) {
+                prositeusersFacade.remove(psu);
+            }
+        }
         siteuserEJB.remove(e);
         lF.create(new Logging(new Date(System.currentTimeMillis()), FacesContext.getCurrentInstance().getExternalContext().getRemoteUser(), getClass().getName(), "INFO", e.getUsername() + " has been deleted."));
         projectListT.remove(e);
-
+        
     }
-
+    
     public void sendInvite(Siteuser su) {
         HttpServletRequest req = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
         String url = req.getRequestURL().toString();
@@ -203,22 +207,22 @@ public class SiteUsersEdit implements Serializable {
             Logger.getLogger(SiteuserControl.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-
+    
     private String encryptPassword(String planepassword) {
         try {
             MessageDigest md = java.security.MessageDigest.getInstance("SHA-256");
             md.update(planepassword.getBytes("UTF-8"));
             byte[] passwordDigest = md.digest();
             String encodedPasswordHash = new sun.misc.BASE64Encoder().encode(passwordDigest);
-
+            
             return encodedPasswordHash;
-
+            
         } catch (NoSuchAlgorithmException | UnsupportedEncodingException ex) {
-
+            
         }
         return "";
     }
-
+    
     private String passGenerator() {
         String dCase = "abcdefghijklmnopqrstuvwxyz";
         String uCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -244,7 +248,7 @@ public class SiteUsersEdit implements Serializable {
         }
         return pass;
     }
-
+    
     private void sendMail(String email, String subject, String body) throws NamingException, javax.mail.MessagingException {
         javax.mail.internet.MimeMessage message = new javax.mail.internet.MimeMessage(mailAOSMail);
         message.setSubject(subject);
@@ -252,159 +256,159 @@ public class SiteUsersEdit implements Serializable {
         message.setText(body);
         javax.mail.Transport.send(message);
     }
-
+    
     public void onEdit(RowEditEvent event) {
-
+        
         try {
-
+            
             Siteuser test = getFacade().find(((Siteuser) event.getObject()).getUsername());
-
+            
             test.setUsername(((Siteuser) event.getObject()).getUsername());
             test.setPassword(((Siteuser) event.getObject()).getPassword());
             test.setFirstname(((Siteuser) event.getObject()).getFirstname());
             siteuserEJB.edit(test);
             lF.create(new Logging(new Date(System.currentTimeMillis()), FacesContext.getCurrentInstance().getExternalContext().getRemoteUser(), getClass().getName(), "INFO", test.getUsername() + " has been edited."));
-
+            
             FacesMessage msg = new FacesMessage();
             msg.setSeverity(FacesMessage.SEVERITY_INFO);
             msg.setSummary("Siteuser edited sucsessfully!");
             msg.setDetail(" ");
-
+            
             FacesContext.getCurrentInstance().addMessage(null, msg);
-
+            
         } catch (Exception e) {
             FacesMessage msg = new FacesMessage();
             msg.setSeverity(FacesMessage.SEVERITY_INFO);
             msg.setSummary("Siteuser not edited!");
             msg.setDetail("Maybe faulty inputs?");
-
+            
             FacesContext.getCurrentInstance().addMessage(null, msg);
-
+            
         }
     }
-
+    
     public void onCancel(RowEditEvent event) {
         FacesMessage msg = new FacesMessage("Editing Cancelled", ((Siteuser) event.getObject()).getUsername());
-
+        
         FacesContext.getCurrentInstance().addMessage(null, msg);
     }
-
+    
     private SiteuserFacade getFacade() {
         return siteuserEJB;
     }
-
+    
     public void onTransferSU(TransferEvent event) {
         StringBuilder builder = new StringBuilder();
         if (event.isAdd()) {
-
+            
             for (Object item : event.getItems()) {
                 Siteuser u = (Siteuser) item;
                 builder.append(u.getUsername()).append("<br />");
-
+                
             }
-
+            
             FacesMessage msg = new FacesMessage();
             msg.setSeverity(FacesMessage.SEVERITY_INFO);
             msg.setSummary("Items Transferred");
             msg.setDetail(builder.toString());
-
+            
             FacesContext.getCurrentInstance().addMessage(null, msg);
-
+            
         } else if (!event.isAdd()) {
             for (Object item : event.getItems()) {
                 Siteuser u = (Siteuser) item;
                 builder.append(u.getUsername()).append("<br />");
-
+                
             }
             FacesMessage msg = new FacesMessage();
             msg.setSeverity(FacesMessage.SEVERITY_INFO);
             msg.setSummary("Items Removed");
             msg.setDetail(builder.toString());
-
+            
             FacesContext.getCurrentInstance().addMessage(null, msg);
         }
-
+        
     }
-
+    
     public void onTransferP(TransferEvent event) {
         StringBuilder builder = new StringBuilder();
         if (event.isAdd()) {
-
+            
             for (Object item : event.getItems()) {
                 Projects pro = (Projects) item;
                 builder.append(pro.getName()).append("<br />");
-
+                
             }
-
+            
             FacesMessage msg = new FacesMessage();
             msg.setSeverity(FacesMessage.SEVERITY_INFO);
             msg.setSummary("Items Transferred");
             msg.setDetail(builder.toString());
-
+            
             FacesContext.getCurrentInstance().addMessage(null, msg);
-
+            
         } else if (!event.isAdd()) {
             for (Object item : event.getItems()) {
                 Projects pro = (Projects) item;
                 builder.append(pro.getName()).append("<br />");
-
+                
             }
             FacesMessage msg = new FacesMessage();
             msg.setSeverity(FacesMessage.SEVERITY_INFO);
             msg.setSummary("Items Removed");
             msg.setDetail(builder.toString());
-
+            
             FacesContext.getCurrentInstance().addMessage(null, msg);
         }
-
+        
     }
-
+    
     public void saveSUP() {
-
+        
         List<Siteuser> su = listSU.getTarget();
         List<Projects> pro = listPro.getTarget();
-
+        
         for (Projects p : pro) {
             for (Siteuser s : su) {
                 prositeusersFacade.create(new Prositeusers(p.getProjectid(), s.getUsername()));
                 lF.create(new Logging(new Date(System.currentTimeMillis()), FacesContext.getCurrentInstance().getExternalContext().getRemoteUser(), getClass().getName(), "INFO", s.getUsername() + " has been added to: " + p.getName() + ""));
             }
         }
-
+        
     }
-
+    
     public String onFlowProcess(FlowEvent event) {
-
+        
         return event.getNewStep();
-
+        
     }
-
+    
     public void onEditPro(RowEditEvent event) {
-
+        
         ProjectUsersModel e = (ProjectUsersModel) event.getObject();
-
+        
         Projects pro = projectsFacade.find(e.getPro().getProjectid());
         pro.setDescription(e.getPro().getDescription());
         pro.setName(e.getPro().getName());
         pro.setProjecttype(e.getPro().getProjecttype());
         projectsFacade.edit(pro);
         lF.create(new Logging(new Date(System.currentTimeMillis()), FacesContext.getCurrentInstance().getExternalContext().getRemoteUser(), getClass().getName(), "INFO", pro.getName() + " has been edited."));
-
+        
         init();
         FacesMessage msg = new FacesMessage("Project Edited", ((ProjectUsersModel) event.getObject()).getPro().getName());
-
+        
         FacesContext.getCurrentInstance().addMessage(null, msg);
-
+        
     }
-
+    
     public void onCancelPro(RowEditEvent event) {
         FacesMessage msg = new FacesMessage("Project Edit Cancelled", ((ProjectUsersModel) event.getObject()).getPro().getName());
-
+        
         FacesContext.getCurrentInstance().addMessage(null, msg);
     }
-
+    
     public void deleteItemPro(ProjectUsersModel e) {
-
+        
         for (Siteuser u : e.getUserList()) {
             prositeusersFacade.remove(new Prositeusers(e.getPro().getProjectid(), u.getUsername()));
             lF.create(new Logging(new Date(System.currentTimeMillis()), FacesContext.getCurrentInstance().getExternalContext().getRemoteUser(), getClass().getName(), "INFO", u.getUsername() + " has been removed: " + e.getPro().getName() + ""));
@@ -423,20 +427,20 @@ public class SiteUsersEdit implements Serializable {
         pF.removeAllPro(e.getPro().getProjectid());
         sruF.removeAllPro(e.getPro().getProjectid());
         sF.removeAllPro(e.getPro().getProjectid());
-
+        
         Projects pro = projectsFacade.find(e.getPro().getProjectid());
         projectsFacade.remove(pro);
         lF.create(new Logging(new Date(System.currentTimeMillis()), FacesContext.getCurrentInstance().getExternalContext().getRemoteUser(), getClass().getName(), "INFO", e.getPro().getName() + " has been deleted."));
         proUsers.remove(e);
-
+        
     }
-
+    
     public void deleteItemProUser(ProjectUsersModel g, Siteuser e) {
-
+        
         prositeusersFacade.remove(new Prositeusers(g.getPro().getProjectid(), e.getUsername()));
         lF.create(new Logging(new Date(System.currentTimeMillis()), FacesContext.getCurrentInstance().getExternalContext().getRemoteUser(), getClass().getName(), "INFO", e.getUsername() + " has been removed: " + g.getPro().getName() + ""));
         int i = proUsers.indexOf(g);
         proUsers.get(i).getUserList().remove(e);
-
+        
     }
 }
