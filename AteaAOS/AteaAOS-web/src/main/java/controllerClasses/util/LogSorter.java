dd7b2bcab2a195fa66e5a4a626f93ea5adf9c94d
simond <simond@trl167057.ka.hist.no>
2014-05-22 13:05:47 +0200

    Merge origin/master
    Conflicts:
    	AteaAOS/AteaAOS-web/src/main/java/entityModels/Users.java

--- AteaAOS/AteaAOS-web/src/main/java/controllerClasses/util/LogSorter.java
+++ AteaAOS/AteaAOS-web/src/main/java/controllerClasses/util/LogSorter.java
@@ -18,6 +18,7 @@
 package controllerClasses.util;
 
 import entityModels.Logging;
+import java.lang.reflect.Field;
 import java.util.Comparator;
 import org.primefaces.model.SortOrder;
 
@@ -39,8 +40,10 @@ public class LogSorter implements Comparator<Logging>{
     @Override
     public int compare(Logging log1, Logging log2) {
         try {
-            Object value1 = Logging.class.getField(this.sortField).get(log1);
-            Object value2 = Logging.class.getField(this.sortField).get(log2);
+            Field field = Logging.class.getDeclaredField(this.sortField);
+            field.setAccessible(true);
+            Object value1 = field.get(log1);
+            Object value2 = field.get(log2);
  
             int value = ((Comparable)value1).compareTo(value2);
              
