fb72096b2f8a8dcc0942e51be70ca76994e724b9
simond <simond@trl167057.ka.hist.no>
2014-05-22 12:56:17 +0200

    Merge origin/master
    Conflicts:
    	AteaAOS/AteaAOS-web/src/main/java/entityModels/Users.java

--- AteaAOS/AteaAOS-web/src/main/java/controllerClasses/special/model/LoggingModel.java
+++ AteaAOS/AteaAOS-web/src/main/java/controllerClasses/special/model/LoggingModel.java
@@ -14,11 +14,11 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package controllerClasses.special.model;
 
 import controllerClasses.util.LogSorter;
 import entityModels.Logging;
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
@@ -31,82 +31,82 @@ import org.primefaces.model.SortOrder;
  *
  * @author simond
  */
-public class LoggingModel extends LazyDataModel<Logging>{
-    
+public class LoggingModel extends LazyDataModel<Logging> {
+
     private List<Logging> datasource;
-     
+
     public LoggingModel(List<Logging> datasource) {
         this.datasource = datasource;
     }
-     
+
     @Override
     public Logging getRowData(String rowKey) {
-        for(Logging log : datasource) {
-            if(log.getDated().equals(rowKey))
+        for (Logging log : datasource) {
+            if (log.getDated().equals(rowKey)) {
                 return log;
+            }
         }
- 
+
         return null;
     }
- 
+
     @Override
     public Object getRowKey(Logging car) {
         return car.getDated();
     }
- 
-    
+
     @Override
-    public List<Logging> load(int first, int pageSize, String sortField, SortOrder sortOrder, Map<String,String> filters) {
+    public List<Logging> load(int first, int pageSize, String sortField, SortOrder sortOrder, Map<String, String> filters) {
         List<Logging> data = new ArrayList<>();
- 
+
         //filter
-        for(Logging log : datasource) {
+        for (Logging log : datasource) {
             boolean match = true;
- 
+
             if (filters != null) {
                 for (Iterator<String> it = filters.keySet().iterator(); it.hasNext();) {
                     try {
                         String filterProperty = it.next();
                         Object filterValue = filters.get(filterProperty);
-                        String fieldValue = String.valueOf(log.getClass().getDeclaredField(filterProperty).get(log));
- 
-                        if(filterValue == null || fieldValue.startsWith(filterValue.toString())) {
+                        Field field = log.getClass().getDeclaredField(filterProperty);
+                        field.setAccessible(true);
+
+                        String fieldValue = String.valueOf(field.get(log));
+
+                        if (filterValue == null || fieldValue.startsWith(filterValue.toString())) {
                             match = true;
-                    }
-                    else {
+                        } else {
                             match = false;
                             break;
                         }
-                    } catch(Exception e) {
+                    } catch (Exception e) {
                         match = false;
                     }
                 }
             }
- 
-            if(match) {
+
+            if (match) {
                 data.add(log);
             }
         }
- 
+
         //sort
-        if(sortField != null) {
+        if (sortField != null) {
             Collections.sort(data, new LogSorter(sortField, sortOrder));
         }
- 
+
         //rowCount
         int dataSize = data.size();
         this.setRowCount(dataSize);
- 
+
         //paginate
-        if(dataSize > pageSize) {
+        if (dataSize > pageSize) {
             try {
                 return data.subList(first, first + pageSize);
-            }
-            catch(IndexOutOfBoundsException e) {
+            } catch (IndexOutOfBoundsException e) {
                 return data.subList(first, first + (dataSize % pageSize));
             }
-        }
-        else {
+        } else {
             return data;
         }
     }
