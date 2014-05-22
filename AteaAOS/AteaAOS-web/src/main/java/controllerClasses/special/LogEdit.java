b8f768d1f015d8e09cf2e674706b1fb82d29e308
simond <simond@trl167057.ka.hist.no>
2014-05-22 16:26:16 +0200

    Merge origin/master
    Conflicts:
    	AteaAOS/AteaAOS-web/src/main/java/entityModels/Users.java

--- AteaAOS/AteaAOS-web/src/main/java/controllerClasses/special/LogEdit.java
+++ AteaAOS/AteaAOS-web/src/main/java/controllerClasses/special/LogEdit.java
@@ -13,8 +13,8 @@ import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
-import org.apache.myfaces.extensions.cdi.core.api.scope.conversation.ViewAccessScoped;
 import org.primefaces.event.SelectEvent;
 import persistClasses.LoggingFacade;
 
@@ -23,34 +23,24 @@ import persistClasses.LoggingFacade;
  * @author simond
  */
 @ManagedBean
-@ViewAccessScoped
-public class LogEdit implements Serializable{
+@ViewScoped
+public class LogEdit implements Serializable {
 
     private LoggingModel loggList;
 
-    private Logging selectedLog;
+   
 
-    
     @EJB
     private LoggingFacade lF;
-  
 
     @PostConstruct
     public void init() {
         loggList = new LoggingModel(lF.findAll());
-       
+
     }
 
     public LoggingModel getLoggList() {
         return loggList;
-    }
-
-    public Logging getSelectedLog() {
-        return selectedLog;
-    }
-
-    public void setSelectedLog(Logging selectedLog) {
-        this.selectedLog = selectedLog;
     }
 
     
@@ -63,7 +53,8 @@ public class LogEdit implements Serializable{
         FacesMessage msg = new FacesMessage("Log Selected", "");
         FacesContext.getCurrentInstance().addMessage(null, msg);
     }
-    public void delLogList(){
+
+    public void delLogList() {
         lF.removeAll();
         lF.edit(new Logging(new Date(System.currentTimeMillis()), FacesContext.getCurrentInstance().getExternalContext().getRemoteUser(), getClass().getName(), "Warning", " The log has been erased."));
         init();
